bb5a6bcd95b8cd4f2e7dc01fe0dd040f0666b00d
valentina-blackledge <107759848+valentina-blackledge@users.noreply.github.com>
2026-03-18 11:55:40 -0400

    Update streamlit_app.py
    import requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,7 @@ if ingredients_list:
     if st.button('Submit Order'):
         cnx._instance.cursor().execute(my_insert_stmt)
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
