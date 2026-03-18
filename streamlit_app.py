d258bef56faefbe3132eff251ce69d6fae3f148c
valentina-blackledge <107759848+valentina-blackledge@users.noreply.github.com>
2026-03-18 09:40:07 -0400

    Update streamlit_app.py
    adding syntax )

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,4 @@ if ingredients_list:
 
     if st.button('Submit Order'):
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅"
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
