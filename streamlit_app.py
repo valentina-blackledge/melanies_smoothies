0e7a063aaef83e39cb5d7e261b973c0731e4d027
valentina-blackledge <107759848+valentina-blackledge@users.noreply.github.com>
2026-03-18 12:06:39 -0400

    Update streamlit_app.py
    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
@@ -19,13 +20,11 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen.strip())
+        st.text(smoothiefroot_response.json())
 
     my_insert_stmt = "INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES ('" + ingredients_string + "', '" + name_on_order + "')"
 
     if st.button('Submit Order'):
         cnx._instance.cursor().execute(my_insert_stmt)
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
